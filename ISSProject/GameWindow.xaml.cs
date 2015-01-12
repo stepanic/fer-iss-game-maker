6a729f7773e18af840c1e559737ef380ead2bcc0
Petar Kovacevic <simunic.petar@gmail.com>
2015-01-12 11:22:57 +0100

    Game window populates secondary screen (if available)

--- ISSProject/GameWindow.xaml.cs
+++ ISSProject/GameWindow.xaml.cs
@@ -27,5 +27,34 @@ namespace ISSProject
             _context = context;
             InitializeComponent();
         }
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+            // Try to position application to first non-primary monitor
+            if (Screen.AllScreens.Length >= 2)
+            {
+                var secondary = 0;
+                for (int index = 0; index < Screen.AllScreens.Length; index++)
+                {
+                    if (Screen.AllScreens[index].Primary) continue;
+                    secondary = index;
+                    break;
+                }
+
+                var screen = Screen.AllScreens[secondary];
+                if (screen != null)
+                {
+                    var area = screen.WorkingArea;
+                    if (!area.IsEmpty)
+                    {
+                        this.Left = area.Left;
+                        this.Top = area.Top;
+                        this.Width = area.Width;
+                        this.Height = area.Height;
+                        this.WindowState = WindowState.Maximized;
+                    }
+                }
+            }
+        }
     }
 }
