ad65bbce5a04e040d82f6b954142e28ac5bece4c
pkovacevic <simunic.petar@gmail.com>
2015-01-14 15:49:51 +0100

    Expanded the model with game in / out models

---
+++ ISSProject/Models/GoNoGoParameters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISSProject.Models
+{
+    public class GoNoGoParameters
+    {
+        public int SectionNum { get; set; }
+        public IList<int> SectionSamplings { get; set; }
+        public IList<TimeSpan> SectionDurations { get; set; } 
+        
+        public GoNoGoParameters()
+        {
+            SectionSamplings = new List<int>();
+            SectionDurations = new List<TimeSpan>();
+        }
+    }
+}
---
+++ ISSProject/Models/GoNoGoResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISSProject.Models
+{
+    public class GoNoGoResult
+    {
+        public int Misses { get; set; }
+        public int Hits { get; set; }
+        public int Errors { get; set; }
+        /// <summary>
+        /// Reaction times in ms
+        /// </summary>
+        public IList<int> ReactionTimes { get; set; }
+
+        public double AverageReactionTime { get { return ReactionTimes.Average(); } }
+
+
+        public GoNoGoResult()
+        {
+            ReactionTimes = new List<int>();
+        }
+    }
+}
