213f48ddf2831bf80cad354b1c427fbc9666407b
Petar Kovačević <petar.kovacevic@axilis.hr>
2015-01-14 14:40:32 +0100

    Added sound model

---
+++ ISSProject/Models/Sound.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ISSProject.Models
+{
+    public class Sound
+    {
+        public MediaPlayer Player { get; set; }
+        public string StimulusGuid { get; set; }
+    }
+}
